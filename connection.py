b07fd4ed0c4df6692e3f2fa9a9f7fd4c7e4f7c2c
Marc Abramowitz <marc@marc-abramowitz.com>
2014-12-02 20:48:04 -0800

    connection.py: Make PEP8 friendly
    Make code easier to read and more Python 3 compatible.

--- connection.py
+++ connection.py
@@ -1,31 +1,27 @@
 import bigsuds
-class Connection:
-  def __init__(self, host, user, password, partition):
-    self.host = host
-    self.user = user
-    self.password = password
-    self.partition = partition
-  
-  def connect(self):
-    """
-        Create a connection to the device.
 
-        @param self - the object type
 
-    """
-    try:
-      b = bigsuds.BIGIP(
-        hostname = self.host, 
-        username = self.user, 
-        password = self.password
-        )
-      try:
-        bclient = b.with_session_id()
-        bclient.System.Session.set_transaction_timeout(60)
-        bclient.System.Session.set_active_folder("/"+self.partition)
-        
-        return bclient
-      except bigsuds.OperationFailed, e: 
-        print e 
-    except bigsuds.ConnectionError as e:
-      raise Exception("Authentication failed")
+class Connection(object):
+
+    def __init__(self, host, user, password, partition):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.partition = partition
+
+    def connect(self):
+        """Create a connection to the device."""
+        try:
+            bigip = bigsuds.BIGIP(
+                hostname=self.host,
+                username=self.user,
+                password=self.password)
+            try:
+                bclient = bigip.with_session_id()
+                bclient.System.Session.set_transaction_timeout(60)
+                bclient.System.Session.set_active_folder("/" + self.partition)
+                return bclient
+            except bigsuds.OperationFailed as e:
+                print(e)
+        except bigsuds.ConnectionError as e:
+            raise Exception("Authentication failed")
