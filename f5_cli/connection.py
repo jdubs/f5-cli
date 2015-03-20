0ea53a9a9a0abf0d549da6d907c53900c3fbc7aa
Marc Abramowitz <marc@marc-abramowitz.com>
2015-03-20 15:43:28 -0700

    --no-verify to turn off SSL cert validation

--- f5_cli/connection.py
+++ f5_cli/connection.py
@@ -3,12 +3,19 @@ import bigsuds
 
 class Connection(object):
 
-    def __init__(self, host, user, password, partition, debug=False):
+    def __init__(self, host, user, password, partition, debug=False, verify=True):
         self.host = host
         self.user = user
         self.password = password
         self.partition = partition
         self.debug = debug
+        if not verify:
+            self.disable_ssl_cert_validation()
+
+    def disable_ssl_cert_validation(self):
+        # From https://www.python.org/dev/peps/pep-0476/#id29
+        import ssl
+        ssl._create_default_https_context = ssl._create_unverified_context
 
     def connect(self):
         """Create a connection to the device."""
