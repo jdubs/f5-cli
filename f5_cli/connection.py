6707c3bfe2e9c97174d737d411c293a8eaebd013
Marc Abramowitz <marc@marc-abramowitz.com>
2015-03-21 08:28:25 -0700

    Less exception catching
    I'm finding the catching of exceptions to be hiding useful info when
    troubleshooting. I like seeing the traceback.

--- f5_cli/connection.py
+++ f5_cli/connection.py
@@ -12,18 +12,12 @@ class Connection(object):
 
     def connect(self):
         """Create a connection to the device."""
-        try:
-            bigip = bigsuds.BIGIP(
-                hostname=self.host,
-                username=self.user,
-                password=self.password,
-                debug=self.debug)
-            try:
-                bclient = bigip.with_session_id()
-                bclient.System.Session.set_transaction_timeout(60)
-                bclient.System.Session.set_active_folder("/" + self.partition)
-                return bclient
-            except bigsuds.OperationFailed as e:
-                print(e)
-        except bigsuds.ConnectionError as e:
-            raise Exception("Authentication failed")
+        bigip = bigsuds.BIGIP(
+            hostname=self.host,
+            username=self.user,
+            password=self.password,
+            debug=self.debug)
+        bclient = bigip.with_session_id()
+        bclient.System.Session.set_transaction_timeout(60)
+        bclient.System.Session.set_active_folder("/" + self.partition)
+        return bclient
